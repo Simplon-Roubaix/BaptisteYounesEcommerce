d4abe4a5fd64e65f733829958046fd8f362c9ab1
geasseur <baptiste.ventura@neuf.fr>
2017-09-26 11:39:12 +0200

    function update call in control pageUtilisateur

--- control/controlPageUtilisateur.php
+++ control/controlPageUtilisateur.php
@@ -5,6 +5,11 @@ $cheminPageUtilisateur = "controlPageUtilisateur.php";
 $chemin_deco = "../index.php";
 saisieSession();
 
+if (isset($_POST['pseudo']) and isset($_POST['password']) and isset($_POST['email']) and isset($_POST['prenom']) and isset($_POST['nom'])) {
+  update($_POST['pseudo'],$_POST['password'],$_POST['email'], $_POST['prenom'],$_POST['nom']);
+}
+
+
 if (isset($_POST['titre']) and isset($_POST['resume']) and isset($_POST['texte']) and isset($_POST['auteur']) and isset($_POST['image'])) {
   ajoutImg($_POST['image']);
   ajoutProduit($_POST['titre'], $_POST['resume'], $_POST['texte'], $_POST['auteur']);
